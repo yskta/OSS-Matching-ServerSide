d092ce84ab7b08c35978ecaa9d9f9e244081be2e
yskta <kitapo3110@gmail.com>
2025-01-06 00:18:46 +0900

    コメント追加

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// インターフェース定義
 type UserRepository interface {
 	Create(db *gorm.DB, user *model.User) (*model.User, error)
 	Get(db *gorm.DB, id string) (*model.User, error)
@@ -13,12 +14,24 @@ type UserRepository interface {
 	Delete(db *gorm.DB, id string) error
 }
 
+// 構造体と初期化
+// 空の構造体userRepositoryを定義し、インターフェースを実装
 type userRepository struct{}
 
+// NewUserRepository()はUserRepositoryインターフェースを実装した新しいインスタンスを返す
 func NewUserRepository() UserRepository {
 	return &userRepository{}
 }
 
+// (r *userRepository): メソッドレシーバー
+// - *userRepository: このメソッドが属する構造体型へのポインタ。これによってこの関数はuserRepository構造体のメソッドとして定義される。
+// 引数
+// - db *gorm.DB: データベースコネクションのポインタ
+// *はポインタを示し、DBインスタンスへの参照を渡す
+// 詳細はhttps://zenn.dev/articles/3e2f3bdc378f19/editでまとめる
+// - user *model.User: 作成するユーザーデータのポインタ
+// model.Userは別パッケージで定義されたUser構造体
+// ポインタを使用することでメモリ効率が良く、データの変更が可能
 func (r *userRepository) Create(db *gorm.DB, user *model.User) (*model.User, error) {
 	if err := db.Create(user).Error; err != nil {
 		return nil, err
